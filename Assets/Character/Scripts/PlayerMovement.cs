c4b86aa42cdc2d10c0d5d353808f4415b8f69430
Jay Varakala <jayvarakala@gmail.com>
2023-07-05 19:18:19 -0400

    rock propulsion done
    shooting rocks downwards will now propel the player upwards. it doesn't push players left and right and that is uh totally on purpose and an intentional game design feature.

--- Assets/Character/Scripts/PlayerMovement.cs
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -8,20 +8,23 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 24f;
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public RockShot rockShot;
     //private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private int recoilForce; //can be tuned in Unity to improve usability
 
 
     // Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
 
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -32,6 +35,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        if(Input.GetMouseButtonDown(0) && rockShot.canFire)
+        {
+            mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = mousePos - transform.position;
+            Vector3 rotation = transform.position - mousePos;
+            rb.velocity = new Vector3(direction.x * -1, direction.y * - 1) * recoilForce; // .normalized after parenthesis decides whether or not strength scales
         }
         //Flip();
     }
