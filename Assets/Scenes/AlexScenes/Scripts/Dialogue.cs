6ffbfbefbbcbd7ae482dd0d770da78ce5b64a83c
Alex Yung <superay190@gmail.com>
2023-07-05 10:37:55 -0400

    dialogue draft commit alex
    started dialogue boxes on the islands

---
+++ Assets/Scenes/AlexScenes/Scripts/Dialogue.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; //to use the textmesh pro component for text
+
+public class Dialogue : MonoBehaviour
+{
+    //reference to the textmeshpro object
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            if(textComponent.text == lines[index])
+            {
+                Nextline();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray()) 
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void Nextline()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
