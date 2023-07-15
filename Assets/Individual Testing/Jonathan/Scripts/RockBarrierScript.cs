48c671745f9e4a06d7b3b3ef7ec2929a44b6e7bb
Alleyoof <52016586+OofCoding@users.noreply.github.com>
2023-07-14 22:25:32 -0400

    more work to starting scene
    useless files were also deleted

---
+++ Assets/Individual Testing/Jonathan/Scripts/RockBarrierScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBarrierScript : MonoBehaviour
+{
+    public GameObject swirl;
+    public GameObject magicRock;
+    public GameObject barrier;
+    public bool isInRange; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrier.SetActive(true);
+        swirl.SetActive(true);
+        magicRock.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange && Input.GetKeyDown("e"))
+        {
+            barrier.SetActive(false);
+            Debug.Log("barrier is gone!");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("something's here?");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("ladder in range");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("something left?");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("ladder not in range");
+        }
+    }
+}
