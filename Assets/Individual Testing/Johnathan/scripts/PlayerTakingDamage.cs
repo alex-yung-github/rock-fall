8138232a6d258119fc286871c2a6b4f4d044c872
Alleyoof <52016586+OofCoding@users.noreply.github.com>
2023-07-20 18:03:10 -0400

    small knockback changes + light npc work

--- Assets/Individual Testing/Johnathan/scripts/PlayerTakingDamage.cs
+++ Assets/Individual Testing/Johnathan/scripts/PlayerTakingDamage.cs
@@ -7,8 +7,12 @@ public class PlayerTakingDamage : MonoBehaviour
     public AttributesManager playerAtm;
     public AttributesManager enemyAtm;
     [SerializeField] private Rigidbody2D rb;
-    public int knockback;
-    public int maxVelocity;
+    // public int knockback;
+    // public int maxVelocity;
+    public float knockbackForce;
+    public float knockbackC;
+    public float knockbackTime;
+    public bool knockFromRight;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -18,13 +22,13 @@ public class PlayerTakingDamage : MonoBehaviour
             else
             {
                 Debug.Log("knockback");
-                if (rb.velocity.x == 0 && rb.velocity.y == 0)
+                if (collision.transform.position.x > transform.position.x)
                 {
-                    rb.velocity = new Vector3(0.2f, 0.2f).normalized * knockback;
+                    rb.velocity = new Vector2(-knockbackForce, knockbackForce);
                 }
-                else if(rb.velocity.x < maxVelocity && rb.velocity.y < maxVelocity)
+                else
                 {
-                    rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y).normalized * knockback;
+                    rb.velocity = new Vector2(knockbackForce, knockbackForce);
                 }
             }
         }
